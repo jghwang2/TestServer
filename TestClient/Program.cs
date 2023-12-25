0207f03b6bcc93964a24003efb79f07e5a831931
Hwang <powerday1988@gmail.com>
2023-12-25 17:17:04 +0900

    서버 리슨, 클라 커맨드로 동작하도록 추가

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -6,7 +6,31 @@ static class Program
 {
     static void Main()
     {
-        var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        client.Connect(new IPEndPoint(IPAddress.Loopback, 20000));
+        while (true)
+        {
+            var command = Console.ReadLine();
+
+            switch(command)
+            {
+                case "connect":
+                    {
+                        var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        var ipAddr = IPAddress.Parse("172.30.1.46");
+                        client.Connect(new IPEndPoint(ipAddr, 2020));
+
+                        if (client.Connected)
+                        {
+                            Console.WriteLine("Connect Success!");
+                        }
+                        
+                        client.Close();
+                        if (false == client.Connected)
+                        {
+                            Console.WriteLine("Close Success!");
+                        }
+                    }
+                    break;
+            }
+        }
     }
 };
--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SuperSocket;
 using SuperSocket.ProtoBase;
 using TestServer;
@@ -9,28 +10,57 @@ static class Program
 {
     static void Main()
     {
-        LogicThread[] logic = new LogicThread[4];
-        var server = SuperSocketHostBuilder.Create<StringPackageInfo, CommandLinePipelineFilter>()
-            .UsePackageHandler(async (s, p) =>
+        var config = new ServerOptions();
+        config.Listeners = new List<ListenOptions>();
+        using (StreamReader file = File.OpenText("config.json"))
+        {
+            using (JsonTextReader reader = new JsonTextReader(file))
             {
-                await Task.Run(() =>
+                var json = (JObject)JToken.ReadFrom(reader);
+                var option = json["serverOptions"];
+                config.Name = option["name"].ToString();
+
+                foreach (var listen in option["listeners"])
                 {
+                    var listener = new ListenOptions();
+                    listener.Ip = listen["ip"].ToString();
+                    listener.Port = (int)listen["port"];
+                    config.AddListener(listener);
+                }
+
+                if (config.Listeners.Count == 0)
                     return;
-                });
-            })
-            .ConfigureLogging((hostCtx, loggingBuilder) =>
-            {
-                loggingBuilder.AddConsole();
-            })
-            .Build();
+            }
+        }
 
-        server.StartAsync();
-
+        LogicThread[] logic = new LogicThread[4];
         for (int i = 0; i < 4; i++)
         {
             logic[i] = new LogicThread();
             logic[i].Start();
         }
+
+        var server = SuperSocketHostBuilder.Create<StringPackageInfo, CommandLinePipelineFilter>()
+            .UsePackageHandler(async (s, p) =>
+            {
+                await Task.Delay(0);
+            })
+            .ConfigureSuperSocket(options =>
+            {
+                options.Name = config.Name;
+                options.Listeners = new List<ListenOptions>();
+
+                foreach(var listen in config.Listeners)
+                {
+                    options.AddListener(new ListenOptions
+                    {
+                        Ip = listen.Ip,
+                        Port = listen.Port
+                    });
+                };
+            }).Build();
+
+        server.StartAsync();
 
         while(true) { }
     }
